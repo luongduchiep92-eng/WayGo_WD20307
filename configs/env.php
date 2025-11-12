5a055d50d139c2e2c396b12b85b51706a53d3629
Luong Duc Hiep <luongduchiep92@gmail.com>
2025-11-12 23:18:35 +0700

    sửa

--- configs/env.php
+++ configs/env.php
@@ -22,7 +22,7 @@ define('DB_HOST',     'localhost');
 define('DB_PORT',     '3306');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME',     '');
+define('DB_NAME',     'tour_managers');
 define('DB_OPTIONS', [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
