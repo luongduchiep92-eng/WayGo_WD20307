0704bde35f6cb7a3f25ae49e4a4abeae8b03f5bc
Luong Duc Hiep <luongduchiep92@gmail.com>
2025-11-12 22:47:43 +0700

    Xóa toàn bộ code cũ

--- assets/js/main.js
+++
@@ -1,6 +0,0 @@
-// You can add custom JavaScript here if needed.
-// For example, handling AJAX form submissions, animations, etc.
-
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('PK-Shop is ready!');
-});
