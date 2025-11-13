9c1093023f1031aab26fda34ad83287cdc282aaf
Luong Duc Hiep <luongduchiep92@gmail.com>
2025-11-13 15:05:11 +0700

    change

--- controllers/TourController.php
+++ controllers/TourController.php
@@ -2,7 +2,12 @@
     class TourController{
         public function listTour(){
             $tourModel = new TourModel();
-            $tours = $tourModel -> getAllTour();
+            $tours = $tourModel -> getAllTours();
             include(PATH_VIEW . 'tours/tour_list.php');
+        }
+        public function addTour(){
+            $tourModel = new TourModel();
+            
+            include(PATH_VIEW . 'tours/tour_add.php');
         }
     }
--- views/tours/tour_list.php
+++ views/tours/tour_list.php
@@ -4,7 +4,7 @@
         <tr>
             <th>ID</th>
             <th>ten_tour</th>
-            <th>cover_image</th>
+            <th>image</th>
             <th>loai_tour</th>
             <th>dia_diem</th>
             <th>thoi_gian</th>
@@ -17,22 +17,22 @@
         </tr>
         <?php foreach($tours as $tour): ?>
             <tr>
-                <td><?= $tour['id'] ?></td>
-                <td><?= $tour['ten_tour'] ?></td>
+                <td><?= $tour->id ?></td>
+                <td><?= $tour->ten_tour ?></td>
                 <td>
-                    <img src="<?= BASE_ASSETS_UPLOADS. $tour['cover_image'] ?>" width="50" alt="">
+                    <img src="<?= BASE_ASSETS_UPLOADS. $tour->image_path ?>" width="50" alt="">
                 </td>
-                <td><?= $tour['loai_tour'] ?></td>
-                <td><?= $tour['dia_diem'] ?></td>
-                <td><?= $tour['thoi_gian'] ?></td>
-                <td><?= number_format($tour['gia_tour']) ?>VND</td>
-                <td><?= $tour['mo_ta'] ?></td>
-                <td><?= $tour['ngay_khoi_hanh'] ?></td>
-                <td><?= $tour['phuong_tien'] ?></td>
-                <td><?= $tour['so_nguoi_toi_da'] ?></td>
+                <td><?= $tour->loai_tour ?></td>
+                <td><?= $tour->dia_diem ?></td>
+                <td><?= $tour->thoi_gian ?></td>
+                <td><?= number_format($tour->gia_tour) ?>VND</td>
+                <td><?= $tour->mo_ta ?></td>
+                <td><?= $tour->ngay_khoi_hanh ?></td>
+                <td><?= $tour->phuong_tien ?></td>
+                <td><?= $tour->so_nguoi_toi_da ?></td>
                 <td>
-                    <a href="<?php echo BASE_URL . '?action=tour-edit&id='.$tour['id'];?>">Sửa</a> |
-                    <a href="<?php echo BASE_URL . '?action=tour-detail&id='.$tour['id'];?>">Chi tiết</a>
+                    <a href="<?php echo BASE_URL . '?action=tour-edit&id='.$tour->id;?>">Sửa</a> |
+                    <a href="<?php echo BASE_URL . '?action=tour-detail&id='.$tour->id;?>">Chi tiết</a>
                 </td>
             </tr>
         <?php endforeach; ?>
