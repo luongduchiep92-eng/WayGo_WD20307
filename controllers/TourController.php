ee9371e2134871530b95ae145d0ec45ef9f6cbb5
Luong Duc Hiep <luongduchiep92@gmail.com>
2025-11-13 19:55:45 +0700

    change

--- controllers/TourController.php
+++ controllers/TourController.php
@@ -2,7 +2,6 @@
     class TourController{
         public function listTour(){
             $tourModel = new TourModel();
-<<<<<<< HEAD
             $tours = $tourModel -> getAllTours();
             include(PATH_VIEW . 'tours/tour_list.php');
         }
@@ -11,9 +10,4 @@
             
             include(PATH_VIEW . 'tours/tour_add.php');
         }
-=======
-            $tours = $tourModel -> getAllTour();
-            include(PATH_VIEW . 'tours/tour_list.php');
-        }
->>>>>>> 75241b5a66087a5b3d6291071368de26c9d794e4
     }
